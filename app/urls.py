bce81b27c0cbd4fa0a182892f060ef8ec9da15e9
Atilio <atiliogoulart@gmail.com>
2021-07-26 13:57:17 -0300

    Add new url

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('app.user.urls'))
+    path('user/', include('app.user.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
