f33107b097d6005ff0571a107210d5aa24ae292c
Atilio <atiliogoulart@gmail.com>
2021-07-27 05:41:55 -0300

    Add user's view

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import NewUserForm
+
+from django.contrib.auth import login
+from django.contrib import messages
+
+def register_request(request):
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.sucess(request, "Registrado com sucesso!")
+            return redirect("main:homapage")
+        else:
+            messages.error(request, "Deu erro, baby!")
+    form = NewUserForm()
+    return render(request=request, template_name="main/register.html", context={"register_form":form})
+    
